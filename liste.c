955d4f0788ce7a3caa4184dd2061a33f819be470
Maximilian Budwill <maximilian.budwill@uni-potsdam.de>
2017-05-24 10:32:14 +0200

    Aufgabenstellung in README eingefügt;

--- liste.c
+++ liste.c
@@ -1,146 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define zv printf("\n");
-
-
 
 struct le {
-int value;
-struct le * next;
+								int value;
+								struct le * next;
 };
-typedef struct le listenelement;
 
+typedef struct le listenelement;
 typedef listenelement * list;
 
+int insert();
+int insert_pos();
 
 
-void insert(int v, list * l){
-    listenelement *new;
-    new= malloc(sizeof(listenelement));
-    new->value= v;
-    new->next= *l;
-    *l =new;
+void insert(int v,list *l){
+								listenelement *new;
+								new = malloc(sizeof(listenelement));
+								new->value =v;
+								new->next=*l;
+								*l=new;
 }
 
+void insert_pos(int v, int pos, list *l){
+								listenelement *new;
+								new = malloc(sizeof(listenelement));
+								new->value =v;
+								new->next =NULL;
+								if(pos==1) {
+																new->next=*l;
+																*l=new;
+								}
+								else{
+																listenelement *temp=*l;
+																int i;
+																for(i=1; i<pos-1; i++) {
+																								temp =temp->next;
+																}
+																new->next = temp->next;
+																temp->next = new;
+								}
 
-int insert_pos(int v, int pos, list * l){
-    if (pos==0){     //falls vorne angehängt wird, kann insert() benutzt werden
-        insert(v,l);
-        return 0;
+
+}
+
+void print_list(list l) {
+								if (l == NULL) printf("leer");
+								else
+																while (l != NULL) {
+																								printf("\n%d ", l->value);
+																								printf("\t%p ", l->next);
+																								l = l->next;
+																}
+}
+
+int length(list l) {
+								int count = 0;
+								while (l != NULL) {
+																count++;
+																l = l->next;
+								}
+								return count;
+}
+
+int delete_head(list * l) {
+								if (*l == NULL) return -1;
+								list old = *l;
+								*l = old->next;
+								free(old);
+								return 0;
+}
+
+void delete_all(list l) {
+								list next;
+								while (l != NULL) {
+																next = l->next;
+																free(l);
+																l = next;
+								}
+}
+
+/*
+   void alles_weg(list *l){
+
+    while(*l != NULL){
+
+        delete_head(l);
     }
-    int count=0; // aktuelle position
-    list  * next=&(*l);
-    while(count < pos && *next!=NULL){
-        next=&(*next)->next;
-        count+=1;
-    } 
-    if (count<pos) return -1;
-    listenelement * new=malloc(sizeof(listenelement));
-    new->value=v;
-    new->next=*next;
-    *next=new;
-            
-    return 0;   
-}
-
-
-
-
-
-int delete_head(list * l){
-
-    if(*l == NULL) return -1;
-    list old =*l;
-    *l=old->next;
-    free(old);
-    return 0;
-}
-
+   }*/
 
 
 void alles_weg(list *l){ //für fehlerbehaftete vorlesungsversion
-    
-    while(*l != NULL){
-    
-        delete_head(l);
-    }    
+								while(*l != NULL) {
+																delete_head(l);
+								}
 }
 
+main(){
+								//listenkopf erstellen
+								listenelement *liste;
+								liste=malloc(sizeof(listenelement));
+								liste->next=NULL;
+								//liste->value=NULL;
 
-void delete_all(list l){
-    list next;
-    while(l!=NULL){
-        next=l->next;
-        free(l);
-        l= next;
-        
-    }
-    
+								printf("\nlistenkopf.next%p",liste->next);
+								printf("\tlistenkopf.value %d",liste->value);
+
+
+								//test length function
+								printf("\nlaenge: %d \n",length(liste));
+								// test insert function
+								insert(1,&liste);
+								insert(5,&liste);
+								printf("\n2 elemente links angefügt:");
+								print_list(liste);
+								//test delete head
+								delete_head(&liste);
+								printf("\nkopf geloescht:");
+								print_list(liste);
+								//test delete_all
+								//delete_all(liste);
+								alles_weg(&liste);
+								printf("\nliste geloescht:");
+								print_list(liste);
+
 }
-
-int length(list l){                
-    int count =0;
-    while(l!=NULL){
-        count++;
-        l=l->next;
-    }
-    return count;
-}
-
-
-void print_list(list l){
-    if(l== NULL) printf("leer");
-    else
-        while (l!= NULL){
-            printf("%d ", l->value);
-            l= l->next;
-        }
-        
-}
-
-
-
-
-int main(){
-    
-    
-   
-    return 0;  
-}    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
