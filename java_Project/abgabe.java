55b4cd5efaa2f556058a5cd5c15df35576c8afce
Ebrantil <maximilian@budwill.de>
2017-07-18 11:27:52 +0200

    Dummy

---
+++ java_Project/abgabe.java
@@ -0,0 +1,9 @@
+// Praxis der Programmierung
+// Hausaufgabe 2 - Java
+// Abgabedatum: 24.7.2017
+//
+// Gruppennummer: 51
+// Gruppenmitglieder:
+// - Georg Duchrau
+// - Nico Maronde
+// - Maximilian Budwill
